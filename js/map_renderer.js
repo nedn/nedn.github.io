585a201d8e9623036ff52e87426b8e8410154a64
nednguyen <nednguyen@google.com>
2015-02-22 11:25:11 -0800

    Update

--- js/map_renderer.js
+++ js/map_renderer.js
@@ -1,10 +1,28 @@
+
 function initialize() {
   var mapCanvas = document.getElementById('map-canvas');
+  var styleArray = [
+  {
+    featureType: "all",
+      stylers: [
+      { saturation: -80 }
+    ]
+  },{
+    featureType: "poi.business",
+      elementType: "labels",
+      stylers: [
+      { visibility: "off" }
+    ]
+  }
+  ];
   var mapOptions = {
     center: new google.maps.LatLng(44.5403, -78.5463),
     zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: styleArray
   }
   var map = new google.maps.Map(mapCanvas, mapOptions)
 }
+
+
 google.maps.event.addDomListener(window, 'load', initialize);
